0f4b28f890fa6d31f29c975f2ffcc5018c7fb64a
Martin Fernandez <martinnfernandezz.20@gmail.com>
2023-12-24 16:12:25 -0300

    Nuevo Carrusel

--- js/cuentaregresiva.js
+++ js/cuentaregresiva.js
@@ -1,33 +1,37 @@
-// const fechaLimiteOferta = new Date(2023, 11, 31, 23, 59, 59);
+const fechaLimiteOferta = new Date(2023, 11, 31, 23, 59, 59);
 
-// function actualizarContador() {
-//   const ahora = new Date().getTime();
-//   const tiempoRestante = fechaLimiteOferta - ahora;
+function actualizarContador() {
+  const ahora = new Date().getTime();
+  const tiempoRestante = fechaLimiteOferta - ahora;
 
-//   if (tiempoRestante > 0) {
-//     const dias = Math.floor(tiempoRestante / (1000 * 60 * 60 * 24));
-//     const horas = Math.floor((tiempoRestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     const minutos = Math.floor((tiempoRestante % (1000 * 60 * 60)) / (1000 * 60));
-//     const segundos = Math.floor((tiempoRestante % (1000 * 60)) / 1000);
+  if (tiempoRestante > 0) {
+    const dias = Math.floor(tiempoRestante / (1000 * 60 * 60 * 24));
+    const horas = Math.floor(
+      (tiempoRestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutos = Math.floor(
+      (tiempoRestante % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const segundos = Math.floor((tiempoRestante % (1000 * 60)) / 1000);
 
-//     document.getElementById('dias').innerText = formatTimeUnit(dias);
-//     document.getElementById('horas').innerText = formatTimeUnit(horas);
-//     document.getElementById('minutos').innerText = formatTimeUnit(minutos);
-//     document.getElementById('segundos').innerText = formatTimeUnit(segundos);
+    document.getElementById("dias").innerText = formatTimeUnit(dias);
+    document.getElementById("horas").innerText = formatTimeUnit(horas);
+    document.getElementById("minutos").innerText = formatTimeUnit(minutos);
+    document.getElementById("segundos").innerText = formatTimeUnit(segundos);
 
-//     setTimeout(actualizarContador, 1000);
-//   } else {
-//     document.querySelector('.contador').innerHTML = '<div class="oferta-finalizada">Oferta Finalizada</div>';
-//   }
-// }
+    setTimeout(actualizarContador, 1000);
+  } else {
+    document.querySelector(".contador").innerHTML =
+      '<div class="oferta-finalizada">Oferta Finalizada</div>';
+  }
+}
 
-// function formatTimeUnit(unit) {
-//   return unit < 10 ? `0${unit}` : unit;
-// }
+function formatTimeUnit(unit) {
+  return unit < 10 ? `0${unit}` : unit;
+}
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   actualizarContador();
-// });
-
+document.addEventListener("DOMContentLoaded", () => {
+  actualizarContador();
+});
 
 /********************************** ESTO NO VA *********************************************/
